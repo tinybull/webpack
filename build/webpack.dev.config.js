a129fb7c629b61ed9dfffa658eeaebfe00d7fcbe
tiny <1064594875@qq.com>
2023-08-24 17:52:48 +0800

    dotenv-webpack 注入客户端代码process.env变量

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const Dotenv  = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,9 +10,6 @@ module.exports = {
     filename: "index.js"
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.NPM_ENV': JSON.stringify(process.env.NPM_ENV),
-    })
+    new Dotenv (),
   ]
 }
